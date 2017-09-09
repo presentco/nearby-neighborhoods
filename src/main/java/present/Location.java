2ae6e04560ef74bb0256826d50491b50412ed439
megha.krishnamurthy <megha.krishnamurthy@glassdoor.com>
2017-09-09 16:48:47 -0700

    Initial set of changes for nearest neighbor searchgs

--- src/main/java/present/Location.java
+++ src/main/java/present/Location.java
@@ -10,6 +10,8 @@ public class Location {
   private final double latitude;
   private final double longitude;
 
+  private static final int ONE_RADIAN_IN_KM = 6371;
+
   /**
    * Constructs a new set of coordinates
    *
@@ -38,7 +40,18 @@ public class Location {
   public double distanceTo(Location other) {
     distanceComputations.incrementAndGet();
 
-    throw new UnsupportedOperationException("Please implement!");
+    double currentLatitude = Math.toRadians(latitude);
+    double currentLongitude = Math.toRadians(longitude);
+    double otherLatitude = Math.toRadians(other.latitude);
+    double otherLongitude = Math.toRadians(other.longitude);
+
+    // Using the cos formula
+    double angleInRadians = Math.acos(
+                      Math.sin(currentLatitude) * Math.sin(otherLatitude) +
+                      Math.cos(currentLatitude) * Math.cos(otherLatitude) * Math.cos(currentLongitude - otherLongitude)
+                    );
+
+    return angleInRadians * ONE_RADIAN_IN_KM;
   }
 
   static AtomicInteger distanceComputations = new AtomicInteger();
--- src/main/java/present/Search.java
+++ src/main/java/present/Search.java
@@ -1,11 +1,14 @@
 package present;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Searches {@link Neighborhoods#ALL}.
  */
 public class Search {
+
+  private static final Map<Location, Map<Double, Neighborhood>> DISTANCE_FROM_LOCATION_TO_NEIGHBORHOODSSORTED_MAP_CACHE = new LinkedHashMap<>();
 
   /**
    * Finds the {@code n} neighborhoods nearest to the given location.
@@ -15,6 +18,58 @@ public class Search {
    * @return the {@code n} nearest neighborhoods, ordered nearest to farthest
    */
   public static List<Neighborhood> near(Location location, int n) {
-    throw new UnsupportedOperationException("Please implement!");
+    Map<Double, Neighborhood> distanceToNeighborhoodsSortedMap = getSortedDistanceToAllLocationsMap(location);
+    List<Neighborhood> nearestNeighborhoodList = new ArrayList<>();
+
+    int counter = 0;
+    for (Neighborhood neighborhood : distanceToNeighborhoodsSortedMap.values()) {
+      nearestNeighborhoodList.add(neighborhood);
+
+      if (++counter >= n) {
+        break;
+      }
+    }
+
+      return nearestNeighborhoodList;
+  }
+
+  private static Map<Double, Neighborhood> getSortedDistanceToAllLocationsMap(Location location) {
+    Map<Double, Neighborhood> distanceToNeighborhoodsSortedMap = DISTANCE_FROM_LOCATION_TO_NEIGHBORHOODSSORTED_MAP_CACHE.get(location);
+
+    if (distanceToNeighborhoodsSortedMap != null) {
+      return distanceToNeighborhoodsSortedMap;
+    }
+
+    distanceToNeighborhoodsSortedMap = getSortedNeighborhoodMapFromLocation(location);
+
+    DISTANCE_FROM_LOCATION_TO_NEIGHBORHOODSSORTED_MAP_CACHE.put(location, distanceToNeighborhoodsSortedMap);
+
+    return distanceToNeighborhoodsSortedMap;
+  }
+
+  private static Map<Double, Neighborhood> getSortedNeighborhoodMapFromLocation (Location location) {
+    Map<Double, Neighborhood> distanceToNeighborhoodsMap = getDistanceToNeighborhoodsMap(location);
+
+    return distanceToNeighborhoodsMap.entrySet().stream()
+            .sorted(Map.Entry.comparingByKey())
+            .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+                    (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+  }
+
+  private static Map<Double, Neighborhood> getDistanceToNeighborhoodsMap(Location location) {
+    Map<Double, Neighborhood> distanceToAllLocationsMap = new HashMap<>();
+
+    for (Neighborhood neighborhood : Neighborhoods.ALL) {
+      Location neighborhoodLocation = neighborhood.location();
+
+      if (location == neighborhoodLocation) {
+        // We do not want to record the current location.
+        continue;
+      }
+
+      distanceToAllLocationsMap.put(location.distanceTo(neighborhoodLocation), neighborhood);
+    }
+
+    return distanceToAllLocationsMap;
   }
 }
