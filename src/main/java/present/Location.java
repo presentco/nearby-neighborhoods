ad63ad0550150ea795d87b00cfca29fa524e7fbd
present <present@presents-MacBook-Pro.local>
2017-09-15 15:31:09 -0700

    Code Challenge Completed (always check your comparators):

--- src/main/java/present/Location.java
+++ src/main/java/present/Location.java
@@ -1,6 +1,7 @@
 package present;
 
 import java.util.concurrent.atomic.AtomicInteger;
+import java.lang.Math;
 
 /**
  * A geographic location
@@ -36,9 +37,29 @@ public class Location {
    * between this location and another in km.
    */
   public double distanceTo(Location other) {
-    distanceComputations.incrementAndGet();
 
-    throw new UnsupportedOperationException("Please implement!");
+    distanceComputations.incrementAndGet();
+    // throw new UnsupportedOperationException("Please implement!");
+
+    // Reference: http://www.movable-type.co.uk/scripts/latlong.html
+    // Reference: http://www.movable-type.co.uk/scripts/gis-faq-5.1.html
+
+    // R is earth's radius in km
+    double R = 6371.0;
+
+    // convert degrees to radians
+    double lat1 = Math.toRadians(this.latitude);
+    double lat2 = Math.toRadians(other.latitude);
+    double lon1 = Math.toRadians(this.longitude);
+    double lon2 = Math.toRadians(other.longitude);
+
+    double deltaLon = lon2 - lon1;
+    double deltaLat = lat2 - lat1;
+    double a = Math.sin(deltaLat/2) * Math.sin(deltaLat/2) + Math.cos(lat1) * Math.cos(lat2) * Math.sin(deltaLon/2) * Math.sin(deltaLon/2);
+    double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    double d = R * c;
+
+    return d;
   }
 
   static AtomicInteger distanceComputations = new AtomicInteger();
