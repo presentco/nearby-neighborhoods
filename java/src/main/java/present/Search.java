96046de0e79f30a132e53bb0f7735e82f67ebea9
Isabel Palomar <isabel.palomarb@gmail.com>
2017-10-30 02:13:46 -0700

    Search.near() implementation to return the neighborhoods closest to a location

--- java/src/main/java/present/Search.java
+++ java/src/main/java/present/Search.java
@@ -1,6 +1,8 @@
 package present;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.PriorityQueue;
 
 /**
  * Searches {@link Neighborhoods#ALL}.
@@ -15,6 +17,37 @@ public class Search {
    * @return the {@code n} nearest neighborhoods, ordered nearest to farthest
    */
   public static List<Neighborhood> near(Location location, int n) {
-    throw new UnsupportedOperationException("Please implement!");
+
+    PriorityQueue<DistanceNeighborhood> nearestNeighborhoods = new PriorityQueue<>();
+
+    for (Neighborhood neighborhood : Neighborhoods.ALL) {
+      addToOrderedList(location, nearestNeighborhoods, neighborhood);
+    }
+
+    return getNearestNeighborhoods(n, nearestNeighborhoods);
+
   }
+
+  private static void addToOrderedList(Location location,
+      PriorityQueue<DistanceNeighborhood> nearestNeighborhoods, Neighborhood neighborhood) {
+
+    Double distanceBetween = location.distanceTo(neighborhood.location());
+    DistanceNeighborhood distanceNeighborhood = new DistanceNeighborhood(distanceBetween,
+        neighborhood);
+
+    nearestNeighborhoods.offer(distanceNeighborhood);
+  }
+
+  private static List<Neighborhood> getNearestNeighborhoods(int n,
+      PriorityQueue<DistanceNeighborhood> nearestNeighborhoods) {
+
+    List<Neighborhood> sortedNeighborhoods = new LinkedList<>();
+
+    for (int i = 0; i < n; i++) {
+      sortedNeighborhoods.add(nearestNeighborhoods.poll().getNeighborhood());
+    }
+
+    return sortedNeighborhoods;
+  }
+
 }
